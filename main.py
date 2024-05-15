054ba39b370a043d786e18c3e053a6c989bdfba8
espensirnes <espen.sirnes@uit.no>
2024-05-15 12:46:40 +0200

    latest

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ import pandas as pd
 import pymssql
 con = pymssql.connect(host='titlon.uit.no', 
 					user="esi000@uit.no", 
-					password  = "YJflxp7elekIVW1dm0HgP",
+					password  = "pw",
 					database='OSE')  
 crsr=con.cursor()
 
